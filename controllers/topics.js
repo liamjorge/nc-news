36e129dc7f41873bc1221749b686cd8624b4911f
Liam Carswell <liamcarswell@gmail.com>
2022-05-16 12:31:54 +0100

    add GET /api/topics endpoint, tests and error handling

---
+++ controllers/topics.js
@@ -0,0 +1,13 @@
+const { selectTopics } = require("../models/topics");
+
+exports.getTopics = () => {};
+
+exports.getTopics = (req, res, next) => {
+  selectTopics()
+    .then((topics) => {
+      res.status(200).send({ topics });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
