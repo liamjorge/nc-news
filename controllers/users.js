977f1ea7a498e3834f0f2fc3a2ece18fb2050ac0
Liam Carswell <liamcarswell@gmail.com>
2022-05-17 11:07:56 +0100

    remove console.log(err)

--- controllers/users.js
+++ controllers/users.js
@@ -5,8 +5,5 @@ exports.getUsers = (req, res, next) => {
     .then((users) => {
       res.status(200).send({ users });
     })
-    .catch((next) => {
-      console.log(err);
-      next(err);
-    });
+    .catch(next);
 };
